e8ad9cd307da877f1c2ffaf41074d411cf7a6204
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2023-06-16 10:30:23 -0700

    update server.js file & pkg.json with dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('This is my server responseeee!');
+    res.end('This is my server response!');
 });
 
 server.listen(process.env.PORT || 3000);
