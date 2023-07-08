6f1279ac772bd7af6a071757131e5b5487a04d84
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2023-07-08 12:50:47 -0700

    Add Mock Update & Delete Routes to app.js

--- backend/app.js
+++ backend/app.js
@@ -76,8 +76,10 @@ app.post('/api/stuff', (req, res, next) => {
 // Route to get a specific thing
 
 app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({_id: req.params.id})
-    .then((thing) => {
+  Thing.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
       if (!thing) {
         return res.status(404).json({ error: 'Thing not found' });
       }
@@ -88,6 +90,47 @@ app.get('/api/stuff/:id', (req, res, next) => {
     });
 });
 
+// Route to update the Thing (corresponding to the object you pass as a first argument. Use the id parameter passed in the request and replace it with the Thing passed as a second argument.
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Thing.updateOne({_id: req.params.id}, thing).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+// Route to delete a Thing
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error
+      });
+    });
+});
 
 // Route to get all things
 app.use('/api/stuff', (req, res, next) => {
