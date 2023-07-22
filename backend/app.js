ca1f16573cef3efabc2b3f163b4ac11cd9c2b28b
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2023-07-22 11:17:18 -0700

    Add Image folder, & configure multer further.

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 const express = require('express'); // Imports the Express framework
 const helmet = require('helmet'); // Imports the helmet middleware for securing HTTP headers
 const path = require('path'); // Imports the path module for working with file and directory paths
-const mongoSanitize = require ('express-mongo-sanitize'); // Imports the mongoSanitize middleware for preventing NoSQL injection attacks
+const mongoSanitize = require('express-mongo-sanitize'); // Imports the mongoSanitize middleware for preventing NoSQL injection attacks
 const sauceRoutes = require('./routes/sauce'); // Imports the sauce routes module
 const userRoutes = require('./routes/user'); // Imports the user routes module
 const app = express(); // Creates an instance of the Express application
@@ -10,29 +10,35 @@ const app = express(); // Creates an instance of the Express application
 
 // EXPRESS SERVER FUNCTIONALITIES
 
-  // Securing HTTP headers
-  app.use(helmet());
+// Securing HTTP headers
+app.use(helmet());
 
-  // Setting headers
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+// Setting headers
+app.use((req, res, next) => { // Middleware to set required headers for allowing cross-origin requests and defining allowed methods and headers.
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-  // Parsing JSON objects
-  app.use(express.json());
+// Parsing JSON objects
+app.use(express.json()); // Middleware for parsing incoming JSON objects in requests
 
-  // Preventing code injection in MongoDB
-  app.use(mongoSanitize());
+// Preventing code injection in MongoDB
+app.use(mongoSanitize()); // Middleware to sanitize user-supplied data and prevent NoSQL injection attacks.
 
-  // Handling image files
-  app.use('/images', express.static(path.join(__dirname, 'images')));
+// Handling image files
+app.use('/images', express.static(path.join(__dirname, 'images'))); // Middleware to serve static files from the 'images' directory.
 
-  // Routes
-  app.use('/api/sauces', sauceRoutes); // Middleware for handling sauce routes
-  app.use('/api/auth', userRoutes); // Middleware for handling user routes
+// Routes
+app.use('/api/sauces', sauceRoutes); // Middleware for handling sauce routes
+app.use('/api/auth', userRoutes); // Middleware for handling user routes
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
 
 // EXPORT
 module.exports = app; // Exports the Express application
--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -11,12 +11,12 @@ const MIME_TYPES = {
 };
 
 // CONFIGURATION
-// Set up disk storage for multer
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
+// Set up disk storage configuration for multer
+const storage = multer.diskStorage({ // Define a disk storage configuration for the Multer middleware using the multer.diskStorage function and assign it to the storage variable
+  destination: (req, file, callback) => { // Defines the destination property of the Multer middleware, where req represents the incoming request, file contains information about the uploaded file, and callback is a function used to indicate the destination directory for storing the uploaded files
     callback(null, 'images'); // Specify the directory where the uploaded files will be stored
   },
-  filename: (req, file, callback) => {
+  filename: (req, file, callback) => { // Defines the filename property of the Multer disk storage, where req represents the incoming request, file contains information about the uploaded file, and callback is a function used to construct the filename for storing the uploaded file
     const name = file.originalname.split(' ').join('_'); // Replace spaces in the original filename with underscores
     const extension = MIME_TYPES[file.mimetype]; // Get the file extension based on the MIME type
     callback(null, name + Date.now() + '.' + extension); // Construct the filename by appending the current timestamp and the file extension
@@ -25,4 +25,4 @@ const storage = multer.diskStorage({
 
 // EXPORT
 // Create and export multer middleware with the configured storage and single file upload
-module.exports = multer({ storage: storage }).single('image');
+module.exports = multer({ storage: storage }).single('image'); // exports a Multer middleware configured to handle single file uploads using the disk storage defined earlier
