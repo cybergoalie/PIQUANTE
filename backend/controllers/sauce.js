1c24e9c51bc3d5b684d0721c85e06067cfebc0de
CyberGoalie <99082706+cybergoalie@users.noreply.github.com>
2023-08-03 15:53:49 -0700

    + sauce controller code comments

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -11,7 +11,7 @@ exports.createSauce = (req, res, next) => {
   let imageUrl = null;
   if (req.file) {
     sauceObject = JSON.parse(req.body.sauce);
-    imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
+    imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`; //Dynamically generate the complete URL by combining the protocol, hostname, and filename using string interpolation with ${} placeholders inside backticks.
   } else {
     sauceObject = req.body;
   }
@@ -19,10 +19,10 @@ exports.createSauce = (req, res, next) => {
   const sauce = new Sauce({
     ...sauceObject,
     imageUrl, // Remove Validators.required from line 59 in the frontend sauce-form-component.ts in order to be able to submit the form without an image affixed... change to image: [null]
-    likes: 0,
-    dislikes: 0,
-    usersLiked: [],
-    usersDisliked: []
+    likes: 0, // This defines a property called likes and initializes it with a value of 0. This property will be used to store the total number of likes for the "Sauce" item. As users like the sauce, this value will be incremented accordingly.
+    dislikes: 0, //  This defines another property called dislikes and initializes it with a value of 0. Similarly, this property will be used to store the total number of dislikes for the "Sauce" item. As users dislike the sauce, this value will be incremented accordingly.
+    usersLiked: [], // This defines an array property called usersLiked.
+    usersDisliked: [] // This defines another array property called usersDisliked. Similarly, when a new "Sauce" object is created, this property will be initialized as an empty array. As users dislike the sauce, their user IDs will be added to this array.
   });
   sauce.save()
     .then(() => res.status(201).json({ message: 'Object saved!' }))
@@ -32,22 +32,22 @@ exports.createSauce = (req, res, next) => {
 // ADD OR REMOVE A LIKE OR DISLIKE
 exports.likeSauce = (req, res, next) => {
   if (req.body.like === 1) {  // Like
-    Sauce.updateOne({ _id: req.params.id }, { $push: { usersLiked: req.body.userId }, $inc: { likes: +1 } })
+    Sauce.updateOne({ _id: req.params.id }, { $push: { usersLiked: req.body.userId }, $inc: { likes: +1 } }) // Update the "Sauce" document with the given ID by adding the user's ID to the "usersLiked" array and incrementing the "likes" count by 1.
       .then(() => res.status(200).json({ message: 'Like added!' }))
       .catch(error => res.status(400).json({ error }));
   } else if (req.body.like === -1) {  // Dislike
-    Sauce.updateOne({ _id: req.params.id }, { $push: { usersDisliked: req.body.userId }, $inc: { dislikes: +1 } })
+    Sauce.updateOne({ _id: req.params.id }, { $push: { usersDisliked: req.body.userId }, $inc: { dislikes: +1 } }) // Update the "Sauce" document with the given ID by adding the user's ID to the "usersDisliked" array and incrementing the "dislikes" count by 1.
       .then(() => res.status(200).json({ message: 'Dislike added!' }))
       .catch(error => res.status(400).json({ error }));
   } else {  // Remove like or dislike
-    Sauce.findOne({ _id: req.params.id })
+    Sauce.findOne({ _id: req.params.id }) // Find the "Sauce" document with the given ID.
       .then(sauce => {
         if (sauce.usersLiked.includes(req.body.userId)) {
-          Sauce.updateOne({ _id: req.params.id }, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
+          Sauce.updateOne({ _id: req.params.id }, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } }) // Update the "Sauce" document with the given ID by removing the user's ID from the "usersDisliked" array and decrementing the "dislikes" count by 1.
             .then(() => res.status(200).json({ message: 'Like removed!' }))
             .catch(error => res.status(400).json({ error }));
         } else if (sauce.usersDisliked.includes(req.body.userId)) {
-          Sauce.updateOne({ _id: req.params.id }, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
+          Sauce.updateOne({ _id: req.params.id }, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } }) // Update the "Sauce" document with the given ID by removing the user's ID from the "usersDisliked" array and decrementing the "dislikes" count by 1.
             .then(() => res.status(200).json({ message: 'Dislike removed!' }))
             .catch(error => res.status(400).json({ error }));
         }
